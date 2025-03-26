ec1354460f2b00c6a1391ce835170641ddb166d5
Pedro Oliveira <pedricky63@gmail.com>
2025-03-26 12:41:55 +0000

    atualização2.1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,18 +12,9 @@ int main() {
     printf("Ex; Carta A01 = Pernambuco, Carta B01 = Bahia! \n");
     printf("\n");
 
-    //Comandos (int's), para selecionar a carta que será usada pelo player.
-    int A01 = 1;
-    int B01 = 2;
-
-    //opção de escolha das cartas para o palyer (Jogador!).
-    printf("Escolha sua carta: ");
-    scanf("%d\n", A01, B01);
-    printf("\n");
-    
     //Códigos usados como base para a funcionabilidade do softwer do jogo.
-    char estado1 = 'A';
-    char estado2 = 'B';
+    char estado1;
+    char estado2;
     char codigo1[10] = "A01";
     char codigo2[10] = "B01";
     char cidade1[20] = "Recife";
@@ -43,41 +34,16 @@ int main() {
     unsigned int poder1 = 2086827;
     unsigned int poder2 = 824605;
     
-    //Comandos para calculos.
+    //Comandos para calcular as densidades e o percapita.
     int soma, subtracao, multiplicacao, divisao;
     
-    int resultado;    
-
-    //inf da carta A01 = PERNAMBUCO.
-    printf("O estado é: %c\n", estado1);
-    printf("O Código é: %s\n", codigo1);
-    printf("A Cidade é: %s\n", cidade1);
-    printf("População: %u\n", populacao1);
-    printf("A Área é: %.2f\n", area1);
-    printf("O P.I.B é: %.2f\n", PIB1);
-    printf("Pontos Turisticos: %d\n", pontos1);
-    printf("A Densidade Populacional é: %f\n", area1 / populacao1);
-    printf("O P.I.B Percapita é: %f\n", PIB1 / populacao1);
-    printf("Super Poder: %d\n", poder1);
-    
-    printf("\n");
-
-    //inf da carta B01 = BAHIA.
-    printf("O estado é: %c\n", estado2);
-    printf("O Código é: %s\n", codigo2);
-    printf("A Cidade é: %s\n", cidade2);
-    printf("População: %u\n", populacao2);
-    printf("A Área é: %.2f\n", area2);
-    printf("O P.I.B é: %.2f\n", PIB2);
-    printf("Pontos Turisticos: %d\n", pontos2);
-    printf("A Densidade Populacional é: %f\n", area2 / populacao2);
-    printf("O P.I.B Percapita é: %f\n", PIB2 / populacao2);
-    printf("Super Poder: %d\n", poder2);
+    int resultado;
 
     printf("\n");
 
-    //comparação das informações da carta A01 e B01.
+    //comparação das informações das cartas.
     printf("Comparação das cartas: \n");
+    printf("\n");
     printf("venceu: %u\n", populacao1 >= populacao2);
     printf("venceu: %d\n", area1 >= area2);
     printf("venceu: %d\n", PIB1 >= PIB2);
@@ -87,12 +53,58 @@ int main() {
     printf("venceu: %d\n", poder1 >= poder2);
 
     printf("\n");
+    printf("Resultado dos atributos! \n");
+    printf("\n");
 
-    //Mensagem de agradecimento!
+    //todos os resultados que o programa gerará.
+    if(populacao1 > populacao2){
+        printf("População: Pernambuco Venceu! \n");
+    }else {
+        printf("População: Bahia venceu! \n");
+    }
+
+    if(area1 > area2){
+        printf("Área: Pernambuco Venceu! \n");
+    }else {
+    printf("Área: Bahia venceu!\n");
+    }
+
+    if(PIB1 < PIB2){
+        printf("P.I.B: Pernambuco Venceu! \n");
+    }else {
+    printf("P.I.B: Bahia venceu! \n");
+    }
+
+    if(pontos1 > pontos2){
+        printf("Pontos Turisticos: Pernambuco Venceu! \n");
+    }else {
+        printf("Pontos Turisticos: Bahia venceu! \n");
+    }
+
+    if(densidade1 > densidade2){
+        printf("Densidade Populacional: Pernambuco Venceu! \n");
+    }else {
+    printf("Densidade Populacional: Bahia venceu!\n");
+    }
+
+    if(densidadeP1 < densidadeP2){
+        printf("Densidade Percapita: Pernambuco Venceu! \n");
+    }else {
+    printf("Densidade Percapita: Bahia venceu! \n");
+    }
+
+    if(poder1 < poder2){
+        printf("Especial: Pernambuco Venceu! \n");
+    }else {
+    printf("Especial: Bahia venceu! \n");
+    }
+
+    //mensagem de agradecimento.
     printf("Muito Obrigado! Seu jogo foi sensacional. \n");
 
     printf("\n");
 
     return 0;
+    
     
 }
